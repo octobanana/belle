c0307a234ea1c70b3aa8abd7386f0627c786188c
Vogl Wolfgang <wolfgang.vogl@d-velop.de>
2018-11-26 08:08:05 +0100

    Fix for the laking generic lambda support in visual studio

--- include/belle.hh
+++ include/belle.hh
@@ -1196,8 +1196,13 @@ private:
 
   private:
 
+#ifdef _MSC_VER
+    template <typename SELF_T, typename RES_T>
+    static constexpr void send(SELF_T self, RES_T&& res)
+#else
     // generic lambda for sending different types of responses
     static auto const constexpr send = [](auto self, auto&& res) -> void
+#endif
     {
       using item_type = typename std::remove_reference<decltype(res)>::type;
 
