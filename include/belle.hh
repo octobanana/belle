a0f2713c19c4bad000296e3d2d7c07a39504ca24
Brett Robinson <octo.banana93@gmail.com>
2018-12-06 12:22:52 -0700

    update version

--- include/belle.hh
+++ include/belle.hh
@@ -21,7 +21,7 @@
                                    OCTOBANANA
 
 Belle
-0.4.0
+0.4.1
 
 An HTTP / Websocket library in C++17 using Boost.Beast and Boost.ASIO.
 https://octobanana.com/software/belle
@@ -53,7 +53,7 @@ SOFTWARE.
 
 #define OB_BELLE_VERSION_MAJOR 0
 #define OB_BELLE_VERSION_MINOR 4
-#define OB_BELLE_VERSION_PATCH 0
+#define OB_BELLE_VERSION_PATCH 1
 
 // Config Begin
 
